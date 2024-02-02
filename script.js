c517ce6aec92dd84a3bc845cfc7601d7b805e8b0
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-02 03:26:57 -0300

    Adding javascript code and fixing bugs.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Variables
-const textInput = document.getElementById('text-input').value;
+const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
 // End of Variables
 
 // Palindrome Checker Funciton
@@ -9,12 +10,24 @@ function Palindrome(str) {
     let normalized = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     let reversed = [...normalized].reverse().join('');
 
-    if (normalized === reversed) {
-
+    if (str === '') {
+        return alert("Please input a value");
+    } else if (normalized === reversed) {
+        result.innerHTML = `
+        <h2> <strong>${str}</strong> is a palindrome.</h2>
+        `
+        return result;
     } else {
-
+        result.innerHTML = `
+        <h2> <strong>${str}</strong> is not a palindrome.</h2>
+        `
+        return result;
     }
 }
 
 // End of Palindrome Checker Funciton
+
+checkBtn.addEventListener('click', () => {
+    Palindrome(textInput.value);
+});
 
